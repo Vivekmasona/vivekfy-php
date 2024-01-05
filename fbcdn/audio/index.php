7900da704329b85be129aa6e5816e4070fb4d200
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-01-05 05:49:22 +0530

    Create index.php

---
+++ fbcdn/audio/index.php
@@ -0,0 +1,28 @@
+<?php
+
+if (isset($_GET['url'])) {
+    $urlParameter = $_GET['url'];
+
+    // Construct the full JSON server URL with the 'url' parameter
+    $jsonServerUrl = 'https://vivekmasona.000webhostapp.com/aa?url=' . urlencode($urlParameter);
+
+    // Fetch JSON data from the server
+    $jsonData = file_get_contents($jsonServerUrl);
+
+    // Decode JSON data
+    $data = json_decode($jsonData, true);
+
+    // Check for errors in the JSON response
+    if (isset($data['error']) && $data['error']) {
+        echo "Error: Unable to fetch audio information.";
+    } else {
+        // Directly redirect to the audio URL
+        header("Location: " . $data['audio']);
+        exit;
+    }
+} else {
+    echo "Error: 'url' parameter is missing.";
+}
+
+?>
+
