1de3a537bb7f09005a671d59f0a78cc74c0fdcbb
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-05-18 20:22:03 +0530

    Create Index.php

---
+++ Get/server/api/Index.php
@@ -0,0 +1,42 @@
+
+<?php
+
+if (isset($_GET['url'])) {
+    $urlParam = $_GET['url'];
+
+    // Extract necessary parts from the provided URL
+    $rapidAPIKey = "11939ea42cmsh9be181f6708fc39p162794jsn9e46f87d898b";
+    $apiHost = "youtube-mp36.p.rapidapi.com";
+    $apiEndpoint = "dl";
+    $url = "https://$apiHost/$apiEndpoint?id=$urlParam";
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0, // Set timeout to 0 for no timeout
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "X-RapidAPI-Host: $apiHost",
+            "X-RapidAPI-Key: $rapidAPIKey"
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo $response;
+    }
+} else {
+    echo "Please provide a valid URL parameter.";
+}
+?>
